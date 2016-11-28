7d759748d0281f0376a638d7ef0f1fff4e373f85
Jacob Hixon and Skye Atwood <skye@>
2016-11-28 10:43:27 -0800

    pass integration specs for telling the user if NOT a triangle

--- spec/triangle_integration_spec.rb
+++ spec/triangle_integration_spec.rb
@@ -36,3 +36,14 @@ describe('triangle path', {:type => :feature}) do
     expect(page).to have_content("It's a scalene!")
   end
 end
+
+describe('triangle path', {:type => :feature}) do
+  it('tells user if their triangle is not a triangle') do
+    visit('/')
+    fill_in('side1', :with => '3')
+    fill_in('side2', :with => '2')
+    fill_in('side3', :with => '10')
+    click_button('Do it!')
+    expect(page).to have_content("It's not a triangle!")
+  end
+end
