b603cfbe4a104276d4eaac7498ab6d6b5a95bae8
Jacob Hixon and Skye Atwood <skye@>
2016-11-28 09:53:32 -0800

    pass spec for checking if triangle is isoceles

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,9 +4,19 @@ class Triangle
     @side2 = side_two
     @side3 = side_three
   end
+
   define_method(:equilateral?) do
     if @side1.==(@side2) && @side2.==(@side3)
       true
     end
   end
+
+  define_method(:isosceles?) do
+    if @side1.==(@side2) && @side1.!=(@side3) || @side1.==(@side3) && @side1.!=(@side2) || @side2.==(@side3) && @side1.!=(@side2)
+      true
+    end
+  end
+
+
+
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -9,4 +9,10 @@ describe(Triangle) do
       expect(new_triangle.equilateral?()).to(eq(true))
     end
   end
+  describe("#isosceles?") do
+    it("checks if two sides are equal") do
+      new_triangle = Triangle.new(8, 5, 5)
+      expect(new_triangle.isosceles?()).to(eq(true))
+    end
+  end
 end
